91ce1dd3aa8c3ddbeaccd4324bd44bafe2ee160f
Your Name <juan.suarez@wipro.com>
2020-08-13 17:33:03 -0500

    Adding test base class

---
+++ AutomationFramework/tests/base_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class BaseTest:
+    @pytest.fixture
+    def create_page_object(self, create_page_object_arg):
+        test_case_file = create_page_object_arg['test_case_file']
+        test_case_name = create_page_object_arg['test_case_name']
+        page_object_class = create_page_object_arg['page_object_class']
+        test_page_object = page_object_class(test_case_file=test_case_file, test_case_name=test_case_name)
+        yield test_page_object
+        test_page_object.clean_after_test()
