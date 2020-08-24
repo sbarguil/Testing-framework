34740d53af2e55100923c6a1565c6a2f6958e3f6
Your Name <juan.suarez@wipro.com>
2020-08-24 18:51:20 -0500

    Tests partially implemented, 1 skipped, 1 remaining

--- AutomationFramework/page_objects/routing_policy/routing_policy.py
+++ AutomationFramework/page_objects/routing_policy/routing_policy.py
@@ -14,6 +14,32 @@ class RoutingPolicy(BasePageObject):
                 'ext_community_set_name': 'routing-policy/defined-sets/bgp-defined-sets/ext-community-sets/ext-community-set/ext-community-set-name',
             }
         ],
+        'rp_policy_def_name': [
+            {
+                'name': 'routing-policy/policy-definitions/policy-definition/name',
+            }
+        ],
+        'rp_policy_def_policy_result': [
+            {
+                'name': 'routing-policy/policy-definitions/policy-definition/name',
+                'statement_name': 'routing-policy/policy-definitions/policy-definition/statements/statement/name',
+                'policy_result': 'routing-policy/policy-definitions/policy-definition/statements/statement/actions/config/policy-result',
+            }
+        ],
+        'rp_policy_def_install_protocol_eq': [
+            {
+                'name': 'routing-policy/policy-definitions/policy-definition/name',
+                'statement_name': 'routing-policy/policy-definitions/policy-definition/statements/statement/name',
+                'install_protocol_eq': 'routing-policy/policy-definitions/policy-definition/statements/statement/conditions/config/install-protocol-eq',
+            }
+        ],
+        'rp_policy_def_match_set_options': [
+            {
+                'name': 'routing-policy/policy-definitions/policy-definition/name',
+                'statement_name': 'routing-policy/policy-definitions/policy-definition/statements/statement/name',
+                'match_set_options': 'routing-policy/policy-definitions/policy-definition/statements/statement/conditions/bgp-conditions/match-ext-community-set/config/match-set-options',
+            }
+        ],
     }
 
     def execute_rp_community_def_edit_config_test_case(self):
@@ -33,5 +59,21 @@ class RoutingPolicy(BasePageObject):
                     </filter>
                     """
         interface_name = self.get_variable_value_for_rpc_in_test_case(rpc_index=0, variable='ext_community_set_name')
+        self.set_filter(filter_to_use.format(interface_name))
+        self.execute_generic_edit_config_test_case()
+
+    def execute_rp_policy_def_edit_config_test_case(self):
+        filter_to_use = """
+                    <filter>
+                        <routing-policy xmlns="http://openconfig.net/yang/routing-policy">
+                            <policy-definitions>
+                              <policy-definition>
+                                <name>{}</name>
+                              </policy-definition>
+                            </policy-definitions>
+                        </routing-policy>
+                    </filter>
+                    """
+        interface_name = self.get_variable_value_for_rpc_in_test_case(rpc_index=0, variable='name')
         self.set_filter(filter_to_use.format(interface_name))
         self.execute_generic_edit_config_test_case()
---
+++ AutomationFramework/tests/routing_policy/test_rp_policy_def.py
@@ -0,0 +1,36 @@
+import pytest
+from AutomationFramework.page_objects.routing_policy.routing_policy import RoutingPolicy
+from AutomationFramework.tests.base_test import BaseTest
+
+
+class TestRPPolicyDef(BaseTest):
+    test_case_file = 'rp_policy_def.yml'
+
+    @pytest.mark.parametrize('create_page_object_arg', [{'test_case_file': test_case_file,
+                                                         'test_case_name': 'rp_policy_def_name',
+                                                         'page_object_class': RoutingPolicy}])
+    def test_rp_policy_def_name(self, create_page_object):
+        create_page_object.execute_rp_policy_def_edit_config_test_case()
+        assert create_page_object.generic_validate_test_case_params(), create_page_object.get_test_case_description()
+
+    @pytest.mark.parametrize('create_page_object_arg', [{'test_case_file': test_case_file,
+                                                         'test_case_name': 'rp_policy_def_policy_result',
+                                                         'page_object_class': RoutingPolicy}])
+    def test_rp_policy_def_policy_result(self, create_page_object):
+        create_page_object.execute_rp_policy_def_edit_config_test_case()
+        assert create_page_object.generic_validate_test_case_params(), create_page_object.get_test_case_description()
+
+    @pytest.mark.parametrize('create_page_object_arg', [{'test_case_file': test_case_file,
+                                                         'test_case_name': 'rp_policy_def_install_protocol_eq',
+                                                         'page_object_class': RoutingPolicy}])
+    def test_rp_policy_def_install_protocol_eq(self, create_page_object):
+        create_page_object.execute_rp_policy_def_edit_config_test_case()
+        assert create_page_object.generic_validate_test_case_params(), create_page_object.get_test_case_description()
+
+    @pytest.mark.skip(reason="Error, tag bgp-conditions not known. ncclient.operations.rpc.RPCError: {'type': 'application', 'tag': 'unknown-element', 'app_tag': None, 'severity': 'error', 'info': '<?xml version=1.0 encoding=UTF-8?><error-info xmlns=urn:ietf:params:xml:ns:netconf:base:1.0><bad-element>match-ext-community-set</bad-element>\n</error-info>\n', 'path': '\n    /rpc/edit-config/config/oc-rpol:routing-policy/oc-rpol:policy-definitions/oc-rpol:policy-definition/oc-rpol:statements/oc-rpol:statement/oc-rpol:conditions/oc-bgp-pol:bgp-conditions\n  ', 'message': None}")
+    @pytest.mark.parametrize('create_page_object_arg', [{'test_case_file': test_case_file,
+                                                         'test_case_name': 'rp_policy_def_match_set_options',
+                                                         'page_object_class': RoutingPolicy}])
+    def test_rp_policy_def_match_set_options(self, create_page_object):
+        create_page_object.execute_rp_policy_def_edit_config_test_case()
+        assert create_page_object.generic_validate_test_case_params(), create_page_object.get_test_case_description()
