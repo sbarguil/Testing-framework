c8894c7cd5c8c36b6aa9b76d9778e9ca5aa7637f
Your Name <juan.suarez@wipro.com>
2020-09-02 00:07:45 -0500

    Modifications needed for tests execution

--- AutomationFramework/utils/rpc_automator_2.py
+++ AutomationFramework/utils/rpc_automator_2.py
@@ -27,7 +27,7 @@ class RPCAutomator2:
                                        hostkey_verify=False,
                                        look_for_keys=False,
                                        allow_agent=False,
-                                       device_params={'name': 'huawei'},
+                                       #device_params={'name': 'huawei'},
                                        )
 
     def rpc_body_generator(self, test_case, rpc_index=0, variables_in_template=None):
