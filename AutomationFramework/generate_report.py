641b5a136a4db39f056a5eaae0b6ffbb96f077ff
sbarguil <samir.barguil@gmail.com>
2020-09-04 10:22:43 +0200

    Reportes

--- AutomationFramework/generate_report.py
+++ AutomationFramework/generate_report.py
@@ -3,10 +3,10 @@
 import sys, os
 import pandas as pd
 
-directory =sys.argv
+directory =sys.argv[1]
 out=pd.DataFrame()
 
-print ("Step #1:")
+print ("Step #1: Find files in:" + directory)
 print ("--------")
 
 for file in os.listdir(directory):
@@ -19,11 +19,13 @@ for file in os.listdir(directory):
             out = pd.concat([out, pd.read_excel(url)],ignore_index=True)
 
 out["TEST-ID (XML_FILE_NAME)"]=out["TEST NAME"].str.replace("test_","").str.split("[").str[0]+'.xml' 
-with pd.ExcelWriter(directory+'total_output.xlsx') as writer:  
-    template.to_excel(writer, sheet_name='Details')
-    out.to_excel(writer, sheet_name='Results')
+template=pd.DataFrame([],columns=['Vendor', 'OS Version','Hardware Version','Date'])
 
-print ("Step #2:")
+with pd.ExcelWriter(directory+'total_output.xlsx') as writer:  
+	out.to_excel(writer, sheet_name='Results')
+	template.to_excel(writer, sheet_name='Details')
+
+print ("Step #2: Composing Results")
 print ("--------")
 
 url1=directory+"total_output.xlsx"
@@ -31,7 +33,6 @@ url2="Params/SBI_params_test_ Almagro_Sept_2020_v1.xlsx"
 #
 file1=pd.read_excel(url1,sheet_name="Results")
 file2=pd.read_excel(url2)
-template=pd.DataFrame([],columns=['Vendor', 'OS Version','Hardware Version','Date'])
 #
 out=pd.merge(file1,file2,on="TEST-ID (XML_FILE_NAME)",how='left')
 out=out[['SUITE NAME','TEST NAME','TEST PARAM','OPENCONFIG PATH','TEST-ID (XML_FILE_NAME)','DESCRIPTION',
