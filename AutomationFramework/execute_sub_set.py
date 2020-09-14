4c4b946cf59d33c260a078e53563677c94739fb0
Your Name <juan.suarez@wipro.com>
2020-09-14 16:04:49 -0500

    Feature  implemented

---
+++ AutomationFramework/execute_sub_set.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tests_sub_set", help="The sub set of tests to execute. It can be a block or a sub-block. i.e. "
+                                              "tests/hardware or tests/hardware/test_hw_component.py")
+    args = parser.parse_args()
+
+    sub_set = args.tests_sub_set
+    print(sub_set)
+
+    # Delete cumulative logs file
+    excel_logs_path = os.path.dirname(os.path.realpath(__file__)) + '/excel_logs/'
+    cumulative_logs_path = excel_logs_path + 'cumulative_log.xlsx'
+    print('Removing file: ' + cumulative_logs_path)
+
+    try:
+        os.remove(cumulative_logs_path)
+    except FileNotFoundError:
+        print('File cummulative_logs.xlxs not found for delete')
+
+    # Run pytest
+    split_sub_set = sub_set.split('/')
+
+    try:
+        while True:
+            split_sub_set.remove('')
+    except:
+        print('Split sub-set clean')
+
+    if len(split_sub_set) == 2:
+        output_file_name = split_sub_set[-1]
+    elif len(split_sub_set) == 3:
+        output_file_name = split_sub_set[-2] + '_' + split_sub_set[-1].split('.')[0]
+    else:
+        raise Exception('Not allowed sub set. Expected notation: tests/hardware or tests/hardware/test_hw_component.py')
+
+    pytest_command = 'py.test ' + sub_set + ' --excelreport=reports/' + output_file_name + '_report.xls'
+
+    print('- Running ' + pytest_command)
+    os.system(pytest_command)
+
+    # Change the cumulative log file name to sub_set file log
+    print('- Rename cummulative_logs.xlxs')
+    os.rename(cumulative_logs_path, excel_logs_path + output_file_name + '_logs.xlsx')
