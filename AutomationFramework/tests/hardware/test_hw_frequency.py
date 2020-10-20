4ecb101dd2871677a0b939e569f0f6c5b2a24b49
Alfredo <alfredo.gonzalezmuniz.ext@telefonica.com>
2020-10-20 11:50:11 +0200

    Añadido tests hw frequency

--- AutomationFramework/tests/hardware/test_hw_frequency.py
+++ AutomationFramework/tests/hardware/test_hw_frequency.py
@@ -7,8 +7,8 @@ class TestHardwareFrequency(BaseTest):
     test_case_file = 'hw_frequency.yml'
 
     @pytest.mark.parametrize('create_page_object_arg', [{'test_case_file': test_case_file,
-                                                         'test_case_name': 'hw_component_frequency',
+                                                         'test_case_name': 'hw_frequency',
                                                          'page_object_class': Hardware}])
-    def test_hw_component_frequency(self, create_page_object):
+    def test_hw_frequency(self, create_page_object):
         create_page_object.execute_get_test_case_with_dispatch()
         assert create_page_object.validate_get_test_case(), create_page_object.get_test_case_description()
